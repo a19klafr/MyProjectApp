4459b704177524888ae2434b7db2d6c70c52df18
Klara Frommelin <a19klafr@student.his.se>
2020-05-21 13:50:18 +0200

    implemented picasso tool to handle images from jsondata

--- app/src/main/java/com/example/myprojectapp/SecondActivity.java
+++ app/src/main/java/com/example/myprojectapp/SecondActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class SecondActivity extends AppCompatActivity {
         TextView textName = findViewById(R.id.text_name);
         TextView textLocation = findViewById(R.id.text_location);
         TextView textLength = findViewById(R.id.text_length);
+        ImageView imageView = findViewById(R.id.imageView);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -25,11 +29,13 @@ public class SecondActivity extends AppCompatActivity {
             String name = extras.getString("name");
             String location = extras.getString("location");
             int length = extras.getInt("length");
+            String image = extras.getString("image");
 
 
             textName.setText(name);
             textLocation.setText(location);
             textLength.setText(String.valueOf(length));
+            Picasso.get().load(image).into(imageView);
         }
 
         Button close = findViewById(R.id.close_details);
